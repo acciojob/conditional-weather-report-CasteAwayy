4d5c0e55f9e40a1db0a2e2cf12608882920ebc59
CasteAwayy <140751958+CasteAwayy@users.noreply.github.com>
2025-01-26 20:53:32 +0530

    Add files via upload

---
+++ src/components/WeatherDisplay.js
@@ -0,0 +1,16 @@
+
+
+export default function WeatherDisplay({ weatherData }) {
+  const { temperature, conditions } = weatherData;
+  return (
+    <div>
+      <p>
+        Temperature:
+        <span style={temperature > 20 ? { color: "red" } : { color: "blue" }}>
+          {temperature}
+        </span>
+      </p>
+      <p>Conditions: {conditions}</p>
+    </div>
+  );
+}
